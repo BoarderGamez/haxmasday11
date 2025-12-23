f49e4bfa6fec437a30be7478b16e4e2eebf3eb1b
BoarderGamez <boardergamezdiscord@gmail.com>
2025-12-24 00:21:24 +0100

    finit I think

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-    loader: glob({ base: './src/content/posts', pattern: '**/*.md' }),
+    loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
     schema: ({ image }) =>
         z.object({
             title: z.string(),
--- src/content/posts/Thissucks.md
+++
@@ -1,9 +0,0 @@
----
-title: Grr, flight,
-description: I'm so tired of this.
-pubDate: 2025-12-24
----
-# Hi jenin
-you may realise its 6am 2h before I leave for my flight, what a man does for an orpheus plushie
-## That's it
-Yes that's really it
